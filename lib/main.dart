994d722d4366a44d44dfe1bf4125ea59a067e950
shahriarshafi <shahriarshafi@gmail.com>
2021-10-23 17:17:55 +0600

    day_2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_18_10_21/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        child: Text("Welcome to $days days of Flutter"),
-      ),
+      home: HomePage(),
     );
   }
 }
